9129b3fd14c08876f3c08129b40912b0d85bd929
Kevin Boyle <papershack@gmail.com>
2019-01-28 17:32:51 -0800

    install react-router-dom w/ Router in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './index.css';
-import App from './App';
+import App from './components/App';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Checkout from './components/Checkout';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import App from '/components/App';
+
+
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route component={App} exact path="/" />
+            <Route component={Signin} path="/signin" />
+            <Route component={Signup} path="/signup" />
+            <Route component={Checkout} path="/checkout" />
+        </Switch>
+    </Router>
+)
+
+
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
