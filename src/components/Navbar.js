f549ceb93de7c83b35f302baea2901e71f5c3604
Kevin Boyle <papershack@gmail.com>
2019-02-01 19:29:49 -0800

    add content to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Box, Text } from 'gestalt'
+import { Box, Text, Heading, Image } from 'gestalt'
 import { NavLink } from 'react-router-dom'
 
 
@@ -7,14 +7,44 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return(
         <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="around"
             height={70}
             color="midnight"
             padding={1}
-            shape="roundedBottom"
         >
+
+            {/* signin link */}
             <NavLink to="/signin">
                 <Text size="xl" color="white">Sign In</Text>
             </NavLink>
+
+            {/* logo & title */}
+            <NavLink to="/">
+                <Box height={50} width={50}>
+                    <Image
+                        alt="Kevin Boyle Art"
+                        naturalHeight={1}
+                        naturalWidth={1}
+                        src="./logo.svg"
+                    />
+                </Box>
+                <Heading size="sx" color="orange">
+                    Kevin Boyle Art
+                </Heading>
+            </NavLink>
+
+
+
+
+
+            {/* signup link */}
+            <NavLink to="/signup">
+                <Text size="xl" color="white">Sign In</Text>
+            </NavLink>
+
+
         </Box>
     )
 }
