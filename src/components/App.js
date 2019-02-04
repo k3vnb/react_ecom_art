c386c7ce08b0d80213c506092787479d78563fb4
Kevin Boyle <papershack@gmail.com>
2019-02-03 20:28:26 -0800

    add styling to App, connect App to backend for rendering content

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
+import { Container, Box, Heading } from 'gestalt'
+import Strapi from 'strapi-sdk-javascript/build/main'
 import './App.css'
 
+const apiUrl = process.env.API_URL || 'http://localhost:1337'
+const strapi = new Strapi(apiUrl);
+
 class App extends Component {
+  async componentDidMount(){
+    const response = await strapi.request('POST', '/graphql', {
+      data: {
+        query: `query {
+          prints {
+            _id
+            name
+            description
+            createdAt
+            image {
+              name
+            }
+          }
+        }`
+      }
+    });
+    console.log(response);
+  }
+
+
   render(){
     return (
-      <div>
-        App
-      </div>
+      <Container>
+        <Box
+          display="flex"
+          justifyContent="center"
+          marginBottom={2}
+          marginTop={12}
+        >
+        {/* Prints Header */}
+        <Heading color="midnight" size="md" >
+          Pop Art, Creative Coding, <br />& Random Visualizations
+        </Heading>
+        </Box>
+      </Container>
     );
   }
 }
